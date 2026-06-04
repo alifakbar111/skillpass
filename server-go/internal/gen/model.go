6eb88865659374811b8ca51e88ff31dc15562e38
alifakbar111 <alifakbar111@gmail.com>
2026-06-04 12:56:09 +0700

    feat: add convenience re-exports for go-jet generated code

---
+++ server-go/internal/gen/model.go
@@ -0,0 +1,18 @@
+package gen
+
+import (
+	"skillpass-server-go/.gen/skillpass/public/model"
+)
+
+//go:generate jet -dsn=postgres://postgres:postgres@localhost:5432/skillpass?sslmode=disable -schema=public -path=../../.gen -ignore-tables=__drizzle_migrations
+
+// Model type aliases for query result mapping.
+type (
+	User              = model.Users
+	Company           = model.Companies
+	JobseekerProfile  = model.JobseekerProfiles
+	JobExperience     = model.JobExperiences
+	IndustryCategory  = model.IndustryCategories
+	Tag               = model.Tags
+	JobPosting        = model.JobPostings
+)
